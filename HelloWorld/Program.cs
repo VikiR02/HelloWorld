1d32b86b01ae55a3ebd0be53e8ccb1a1767c9813
Vika <vikarogova24.12@gmail.com>
2023-11-22 22:22:20 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Задание 1
+            Console.WriteLine("Hello World!!!");
+
+            //Задание 2
+            Console.Write("Нello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
